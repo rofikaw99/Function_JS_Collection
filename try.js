5e33940950706b77b829887426066dfa109b4e7f
rofik.awaludin <rofik.awaludin@edenfarm.id>
2023-05-14 17:48:25 +0700

    update 11052023 1748

--- try.js
+++ try.js
@@ -5,8 +5,31 @@ let b = 45
 function penjumlahan(a, b) {
   return a + b;
 }
-
 console.log('hasil penjumlahan = '+ penjumlahan(a, b));
+
+//perpangkatan
+
+function perpangkatan(a, b) {
+  return Math.pow(a, b);
+}
+console.log('hasil perpangkatan = '+ perpangkatan(a, b));
+
+//akar kuadrat
+
+function akarKuadrat(a) {
+  return Math.sqrt(a);
+}
+
+console.log('hasil akar kuadrat dari '+ b +' = '+ akarKuadrat(b));
+
+
+//ganjil genap
+function ganjilGenap(num){
+  if ( num % 2 === 0){ console.log( num + ' bilangan genap.')  }
+  else {console.log(num + ' bilangan ganjil.')} 
+}
+ganjilGenap(5);
+ganjilGenap(6);
 
 //Mencari angka terbesar
 
@@ -25,7 +48,7 @@ console.log('angka yang terbesar yaitu ' + angkaTerbesar(arr));
 
 //membalik string
 
-let str = 'rofik awaludin'
+let str = 'Rofik Awaludin a QA Automation Engineer'
 function terbalik(str){
   let hasil = '';
   for (let i = str.length - 1; i >= 0; i --){
@@ -39,6 +62,20 @@ function reverseString(str){
   return str.split('').reverse().join('')
 }
 console.log('reverse string = '+ reverseString(str));
+
+function isPalindrome(str){
+  str = str.replace(/\s/g,'').toLowerCase();
+  var reverseString = str.split('').reverse().join('');
+
+  if (str === reverseString){
+    console.log(str + ' is palindrome');
+  } else {
+    console.log(str + ' not palindrome');
+  }
+}
+isPalindrome("level"); // Output: level adalah palindrome
+isPalindrome("A man a plan a canal Panama"); // Output: katak adalah palindrome
+isPalindrome("rumah"); // Output: rumah bukan palindrome
 
 //membandingkan array dengan angka
 let angka = 5;
@@ -128,3 +165,51 @@ function findTriplets(arr){
 }
 
 console.log('menjumlahkan tiga angka dalam array apakah hasilnya 0 True/False maka hasilnya = ' + findTriplets(arr));
+
+// menghitung jumlah huruf kapital
+
+function countCase(str){
+  let upperCase = 0;
+  let lowerCase = 0;
+
+  for (i = 0; i < str.length; i++){
+    if (str[i] === str[i].toUpperCase()){
+      upperCase++;
+    } else if (str[i] === str[i].toLowerCase()){
+      lowerCase++;
+    }
+  }
+  return {upperCase, lowerCase};
+}
+
+console.log(countCase(str));
+
+// pewarisan
+
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+}
+
+Person.prototype.greet = function() {
+  console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
+}
+
+function Student(name, age, grade) {
+  Person.call(this, name, age);
+  this.grade = grade;
+}
+
+Student.prototype = Object.create(Person.prototype);
+Student.prototype.constructor = Student;
+
+Student.prototype.showGrade = function() {
+  console.log(`My grade is ${this.grade}`);
+}
+
+let person1 = new Person('Rofik', 17);
+let student1 = new Student('Udin', 20, 'S');
+
+person1.greet(); // Output: Hello, my name is John and I am 25 years old.
+student1.greet(); // Output: Hello, my name is Jane and I am 20 years old.
+student1.showGrade(); // Output: My grade is A
